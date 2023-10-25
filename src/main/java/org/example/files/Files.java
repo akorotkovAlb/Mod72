13583addd6d618712dfee27625415c61adcf81c1
andrii <korotkovandrey3@gmail.com>
2023-10-25 18:46:21 +0300

    initial commit

--- src/main/java/org/example/files/Files.java
+++ src/main/java/org/example/files/Files.java
@@ -9,7 +9,7 @@ import java.util.Base64;
 public class Files {
 
     public String encodeFile(String fileName) throws Exception {
-        File image = new File("image.jpg");
+        File image = new File(fileName);
         InputStream is = new FileInputStream(image);
         byte[] byteArray = is.readAllBytes();
         String base64String = Base64.getEncoder().encodeToString(byteArray);
